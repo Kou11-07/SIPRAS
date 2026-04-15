56813ffed536dccbf72729220f2bf931206b8b6c
Kiki_Hadiansah <koustar956@gmail.com>
2026-04-15 07:14:54 +0700

    ganti

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         // Buat Admin
         User::create([
             'nisn' => '1234567890',
-            'name' => 'Admin Sekolah',
+            'username' => 'Admin Sekolah',
             'tanggal_lahir' => '1990-01-01',
             'password' => Hash::make('admin123'),
             'email' => 'admin@sekolah.com',
@@ -23,69 +23,5 @@ class DatabaseSeeder extends Seeder
             'role' => 'admin',
             'is_active' => true
         ]);
-
-        // Buat User/Siswa Contoh
-        User::create([
-            'nisn' => '9876543210',
-            'name' => 'Budi Santoso',
-            'tanggal_lahir' => '2005-05-15',
-            'password' => Hash::make('siswa123'),
-            'email' => 'budi@student.com',
-            'phone' => '081298765432',
-            'kelas' => '10 IPA 1',
-            'role' => 'user',
-            'is_active' => true
-        ]);
-
-        // Buat Kategori Sarana
-        $kategoriList = [
-            'Kursi dan Meja',
-            'Papan Tulis',
-            'Proyektor/LCD',
-            'Komputer/Lab',
-            'AC/Kipas Angin',
-            'Lampu Penerangan',
-            'Toilet Sanitasi',
-            'Peralatan Olahraga',
-            'Buku Perpustakaan',
-            'Lainnya'
-        ];
-
-        foreach ($kategoriList as $kategori) {
-            KategoriSarana::create([
-                'nama' => $kategori,
-                'deskripsi' => 'Kategori sarana ' . $kategori,
-                'is_active' => true
-            ]);
-        }
-
-        // Buat Lokasi
-        $lokasiList = [
-            'Ruang Kelas 10 IPA 1',
-            'Ruang Kelas 10 IPA 2',
-            'Ruang Kelas 11 IPA 1',
-            'Ruang Kelas 11 IPA 2',
-            'Ruang Kelas 12 IPA 1',
-            'Ruang Kelas 12 IPA 2',
-            'Laboratorium IPA',
-            'Laboratorium Komputer',
-            'Perpustakaan',
-            'Lapangan Olahraga',
-            'Kantin Sekolah',
-            'Toilet Umum',
-            'Mushola',
-            'Aula Sekolah',
-            'Ruang Guru',
-            'Ruang Kepala Sekolah',
-            'Lainnya'
-        ];
-
-        foreach ($lokasiList as $lokasi) {
-            Lokasi::create([
-                'nama' => $lokasi,
-                'deskripsi' => 'Lokasi ' . $lokasi,
-                'is_active' => true
-            ]);
-        }
     }
 }
