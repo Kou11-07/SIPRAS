220df9c9f1d39156bcfe5f294c0a7c6655f2121e
Kou <koustar956@gmail.com>
2026-04-15 07:46:30 +0700

    back

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,18 +10,10 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run(): void
+    public function run()
     {
-        // Buat Admin
-        User::create([
-            'nisn' => '1234567890',
-            'username' => 'Admin Sekolah',
-            'tanggal_lahir' => '1990-01-01',
-            'password' => Hash::make('admin123'),
-            'email' => 'admin@sekolah.com',
-            'phone' => '081234567890',
-            'role' => 'admin',
-            'is_active' => true
+        $this->call([
+            AdminUserSeeder::class,
         ]);
     }
 }
