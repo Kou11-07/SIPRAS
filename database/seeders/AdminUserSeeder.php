11efde45ab9642ba82a0c4d2130a7e9cb4f61afa
Kou <koustar956@gmail.com>
2026-04-15 08:14:59 +0700

    admin

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserSeeder extends Seeder
 {
@@ -17,7 +18,7 @@ class AdminUserSeeder extends Seeder
             'nisn' => '555555555',
             'username' => 'Admin Sekolah',
             'tanggal_lahir' => '1990-01-01',
-            'password' => 'admin123',
+            'password' => Hash::make('admin123'),
             'email' => 'admin@sekolah.com',
             'phone' => '081234567890',
             'role' => 'admin',
